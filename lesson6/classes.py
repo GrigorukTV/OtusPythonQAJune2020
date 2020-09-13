c5fe61dc5ffd4fa80908a80e23ae3ed0187436c7
Mikhail <dadykinm@yandex.ru>
2020-09-13 21:24:13 +0300

    Доработка д/з по ООП

--- lesson6/classes.py
+++ lesson6/classes.py
@@ -4,22 +4,20 @@ class Figure:
     area = None
     perimeter = None
 
-    def __init__(self, name, area, perimeter):
+    def __init__(self, name):
         self.name = name
-        self.area = area
-        self.perimeter = perimeter
 
     def setName(self, name):
         self.name = name
 
-    def setArea(self, area):
-        self.area = area
-
-    def setPerimeter(self, perimeter):
-        self.perimeter = perimeter
-
     def getAngles(self):
         return self.angles
+
+    def calcArea(self):
+        pass
+
+    def calcPerimeter(self):
+        pass
 
     def add_square(self, cl):
         try:
@@ -29,17 +27,71 @@ class Figure:
             print('Ошибка')
 
 
+
 class Triangle(Figure):
     angles = 3
+    side1 = None
+    side2 = None
+    side3 = None
+    height = None
+
+    def setSides(self, side1, side2, side3 ):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def setHeight(self, height):
+        self.height = height
+
+    def calcArea(self):
+        self.area = 1/2 * self.height * self.side2
+
+    def calcPerimeter(self):
+        self.perimeter = self.side1 + self.side2 + self.side3
 
 
 class Square(Figure):
-   angles = 4
+    angles = 4
+    side = None
+
+    def setSideSquare(self, side):
+        self.side = side
+
+    def calcArea(self):
+        self.area = self.side**2
+
+    def calcPerimeter(self):
+        self.perimeter = 4 * self.side
+
 
 
 class Rectangle(Figure):
-   angles = 4
+    angles = 4
+    side1 = None
+    side2 = None
+
+    def setSideRectangle(self, side1, side2):
+       self.side1 = side1
+       self.side2 = side2
+
+    def calcArea(self):
+        self.area = self.side1 * self.side2
+
+    def calcPerimeter(self):
+        self.perimeter = 2 * (self.side1 + self.side2)
 
 
 class Circle(Figure):
-   angles = 0
+    angles = 0
+    radius = None
+
+    def radiusCircle(self, radius):
+       self.radius = radius
+
+    def calcArea(self):
+       self.area = 3.14 * (self.radius ** 2)
+
+    def calcPerimeter(self):
+        self.perimeter = 2 * 3.14 * self.radius
+
+
--- lesson6/index.py
+++ lesson6/index.py
@@ -2,57 +2,25 @@ import pytest
 
 from .classes import Triangle, Square, Rectangle, Circle
 
-test_data = [(Square,'Square', 1, 3),
-             (Triangle, 'Triangle', 2, 8),
-             (Rectangle, 'Rectangle', 3, 8),
-             (Circle, 'Circle', 4, 8)]
+test_data = [(Square,'Square'),
+             (Triangle, 'Triangle'),
+             (Rectangle, 'Rectangle'),
+             (Circle, 'Circle')]
 
 # создание объекта класса с параметрами и проверка, что свойства устанавливаются корректно
 @pytest.mark.parametrize("param_figure", test_data)
 def test_param_figure(param_figure):
-
     cl = param_figure[0]
     n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
-
+    figure_test = cl(n)
     assert figure_test.name == n
-    assert figure_test.area == a
-    assert figure_test.perimeter == p
-
-# проверка: площадь фигуры > 0
-@pytest.mark.parametrize("param_figure", test_data)
-def test_area (param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
-    assert figure_test.area > 0
-
-# проверка: периметр фигуры > 0
-@pytest.mark.parametrize("param_figure", test_data)
-def test_perimeter (param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
-    assert figure_test.perimeter > 0
 
 # проверка: тип параметра 'name' = str
 @pytest.mark.parametrize("param_figure", test_data)
 def test_name (param_figure):
     cl = param_figure[0]
     n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
+    figure_test = cl(n)
     assert (type(figure_test.name) is str) == True
 
 # проверка количетсва углов в фигуре
@@ -63,61 +31,170 @@ def test_name (param_figure):
 def test_angles(angles_figure):
     cl = angles_figure [0]
     sum_angles = angles_figure [1]
-    figure_test = cl('', 0, 0)
+    figure_test = cl('')
     assert figure_test.angles == sum_angles
 
 
-# проверка: сумма площадей фигур > 0
-@pytest.mark.parametrize("param_figure", test_data)
-def test_add_square(param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
+# проверка: площадь фигур
+# площадь треугольника > суммы сторон треугольника
+test_data_Triangle = [('Triangle1',3,1,1,11),
+             ('Triangle2',23,33,44,55),
+             ('Triangle3',5,6,3,8),
+             ('Triangle4',9,4,4,20)]
 
-    figure_test = cl(n, a, p)
-    figure_test_add = Triangle('Triangle', 2, 8)
-    s = figure_test.add_square(figure_test_add)
-    assert s > 0
 
-# проверка: тип сумма площадей фигур = int
-@pytest.mark.parametrize("param_figure", test_data)
-def test_add_square_type (param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
+@pytest.mark.parametrize("param_figure", test_data_Triangle)
+def test_area_t (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+    s2 = param_figure[2]
+    s3 = param_figure[3]
+    h = param_figure[4]
 
-    figure_test = cl(n, a, p)
-    figure_test_add = Triangle('Triangle', 2, 8)
-    s = figure_test.add_square(figure_test_add)
-    assert (type(s) is int) == True
+    tri = Triangle(n)
+    tri.setSides(s1, s2, s3)
+    tri.setHeight(h)
+
+    tri.calcArea()
+    assert tri.area > s1 + s2 + s3
+
+
+# площадь квадрата > стороны квадрата
+test_data_Square = [('Square3',5),
+                    ('Square4',9)]
+
+@pytest.mark.parametrize("param_figure", test_data_Square)
+def test_area_s (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+
+    squ = Square(n)
+    squ.setSideSquare(s1)
+
+    squ.calcArea()
+    assert squ.area > s1
+
+# площадь прямоугольника > суммы сторон прямоугольника
+test_data_Rectangle= [('Rectangle1',11,12),
+             ('Rectangle2',23,33),
+             ('Rectangle3',5,6),
+             ('Rectangle4',9,8)]
+
+@pytest.mark.parametrize("param_figure", test_data_Rectangle)
+def test_area_r (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+    s2 = param_figure [2]
+
+    rec = Rectangle(n)
+    rec.setSideRectangle(s1, s2)
+
+    rec.calcArea()
+    assert rec.area > s1 + s2
+
+# площадь круга > радиуса круга
+test_data_Circle = [('Circle1',11),
+             ('Circle2',23),
+             ('Circle3',5),
+             ('Circle4',9)]
+
+@pytest.mark.parametrize("param_figure", test_data_Circle)
+def test_area_c (param_figure):
+    n = param_figure[0]
+    r = param_figure[1]
+
+    circ = Circle(n)
+    circ.radiusCircle(r)
+
+    circ.calcArea()
+    assert circ.area > r
+# проверка: периметр фигур
+# периметр треугольника > одной стороны треугольника
+test_data_Triangle = [('Triangle1',3,1,1),
+             ('Triangle2',23,33,44),
+             ('Triangle3',5,6,3),
+             ('Triangle4',9,4,4)]
+
+
+@pytest.mark.parametrize("param_figure", test_data_Triangle)
+def test_perimeter_t (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+    s2 = param_figure[2]
+    s3 = param_figure[3]
+
+    tri = Triangle(n)
+    tri.setSides(s1, s2, s3)
+
+    tri.calcPerimeter()
+    assert tri.perimeter > s1
+
+
+# периметр квадрата > стороны квадрата
+test_data_Square = [('Square1',11),
+             ('Square2',23),
+             ('Square3',5),
+             ('Square4',9)]
+
+@pytest.mark.parametrize("param_figure", test_data_Square)
+def test_perimeter_s (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+
+    squ = Square(n)
+    squ.setSideSquare(s1)
+
+    squ.calcPerimeter()
+    assert squ.perimeter > s1
+
+# периметр прямоугольника > стороны прямоугольника
+@pytest.mark.parametrize("param_figure", test_data_Rectangle)
+def test_perimeter_r (param_figure):
+    n = param_figure[0]
+    s1 = param_figure[1]
+    s2 = param_figure [2]
+
+    rec = Rectangle(n)
+    rec.setSideRectangle(s1, s2)
+
+    rec.calcPerimeter()
+    assert rec.perimeter > s1
+
+# периметр круга > радиуса круга
+@pytest.mark.parametrize("param_figure", test_data_Circle)
+def test_perimeter_c (param_figure):
+    n = param_figure[0]
+    r = param_figure[1]
+
+    circ = Circle(n)
+    circ.radiusCircle(r)
+
+    circ.calcPerimeter()
+    assert circ.perimeter > r
+
 
 # проверка: сумма площадей фигур > площади фигуры
-@pytest.mark.parametrize("param_figure", test_data)
+
+test_data_compare_areas = [({'side1': 2, 'side2': 3, 'side3': 4, 'height': 5}, {"side": 6}, {"radius": 7})]
+@pytest.mark.parametrize("param_figure", test_data_compare_areas)
 def test_add_square_area(param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
-    figure_test_add = Triangle('Triangle', 2, 8)
-    s = figure_test.add_square(figure_test_add)
-    assert s > a
-
-# проверка сеттера
-@pytest.mark.parametrize("param_figure", test_data)
-def test_name (param_figure):
-    cl = param_figure[0]
-    n = param_figure[1]
-    a = param_figure[2]
-    p = param_figure[3]
-
-    figure_test = cl(n, a, p)
-    figure_test.setName('Test')
-    assert figure_test.name == 'Test'
+    firstClassParams = param_figure[0]
+    secondClassParams = param_figure[1]
+    thirdClassParams = param_figure[2]
 
 
+    tri = Triangle('Треугольник')
+    tri.setSides(firstClassParams['side1'], firstClassParams['side2'], firstClassParams['side3'])
+    tri.setHeight(firstClassParams['height'])
+    tri.calcArea()
 
+    squ = Square('Квадрат')
+    squ.setSideSquare(secondClassParams['side'])
+    squ.calcArea()
 
+    cir = Circle('Круг')
+    cir.radiusCircle(thirdClassParams['radius'])
+    cir.calcArea()
+
+    assert tri.add_square(squ) == tri.area + squ.area
+    assert cir.add_square(tri) == tri.area + cir.area
